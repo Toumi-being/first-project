62c832d8c0aa3e74f6a2e6bdde8de549b8464290
Toumi-being <fahmidatabassumtoumi@gmail.com>
2025-09-19 10:06:33 +0600

    First commit from codeblocks

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Hello World message
+    cout << "Hello, World!" << endl;
+
+    // Simple input/output
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "You entered: " << num << endl;
+
+    // Simple calculation
+    int square = num * num;
+    cout << "Square of the number: " << square << endl;
+
+    return 0;
+}
